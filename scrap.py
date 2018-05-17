2f981b2a3da50dd6d81847d2d3c5123ce43d654d
kamroz <37931244+kamroz@users.noreply.github.com>
2018-05-17 07:56:55 +0200

    Create scrap.py

---
+++ scrap.py
@@ -0,0 +1,37 @@
+import urllib.request
+
+def scraphtml (url):
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        return html.decode('utf-8')
+
+
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
+
+
+def allerrand(baseurl):
+    errand = str('<a href="/zlecenie/')
+    print("przed!")
+    html = scraphtml(baseurl)
+    print("po!")
+    #wypisz na ekran pozycję pierwszej litery z łańcucha znaków ze zmiennej `errand`
+    #print(html.find(errand))
+
+    errandlist = list(find_all(html, errand))
+    szukaj = ".html"
+
+    for x in errandlist:
+        znakmniejszosci = x
+        backslash = (znakmniejszosci + errand.__len__())
+        czeschtmla = html[backslash:backslash+200]
+        koniecurla = czeschtmla.find(szukaj)
+
+        #od pozycji `backslash` do pozycji znaku cudzyslowie `"`
+        pozycjakoncaurla = koniecurla + szukaj.__len__()
+        print("http://favore.pl/zlecenie/" + html[backslash :(backslash+pozycjakoncaurla)])
